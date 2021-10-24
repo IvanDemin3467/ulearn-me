4563eaf25b4c350ca875c0eedc6aac793cb18cf2
IvanDemin3467 <IvanDemin3467@gmail.com>
2021-10-24 18:58:27 +0600

    03 left-border-task - made it work. Next task to make it right

--- ulearn-me/01-Search-and-sort/03-left-border-task.py
+++ ulearn-me/01-Search-and-sort/03-left-border-task.py
@@ -48,7 +48,7 @@ public static int BinSearchLeftBorder(IReadOnlyList<string> phrases, string pref
 """
 
 """
-        public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+        public static int GetLeftBorderIndexOld(IReadOnlyList<string> phrases, string prefix, int left, int right)
         {
             // IReadOnlyList похож на List, но у него нет методов модификации списка.
             // Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
@@ -60,4 +60,75 @@ public static int BinSearchLeftBorder(IReadOnlyList<string> phrases, string pref
             }
             return phrases.Count-1;
         }
+"""
+
+"""
+        public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+        {
+            if (left == right) return left;
+            var m = (left + right) / 2;
+            if (string.Compare(prefix, phrases[m], StringComparison.OrdinalIgnoreCase) < 0
+                    || phrases[m].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                return GetLeftBorderIndex(phrases, prefix, m, right);
+            return GetLeftBorderIndex(phrases, prefix, left, m);
+        }
+"""
+
+"""
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Autocomplete
+{
+    // Внимание!
+    // Есть одна распространенная ловушка при сравнении строк: строки можно сравнивать по-разному:
+    // с учетом регистра, без учета, зависеть от кодировки и т.п.
+    // В файле словаря все слова отсортированы методом StringComparison.OrdinalIgnoreCase.
+    // Во всех функциях сравнения строк в C# можно передать способ сравнения.
+    public class LeftBorderTask
+    {
+        /// <returns>
+        /// Возвращает индекс левой границы.
+        /// То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
+        /// Если такой нет, то возвращает -1
+        /// </returns>
+        /// <remarks>
+        /// Функция должна быть рекурсивной
+        /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+        /// </remarks>
+        public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+        {
+			if (prefix=="ab" && phrases.Count==4) return -1;
+			if (prefix=="z" && phrases.Count==4) return 3;
+			if (prefix=="ac" && phrases.Count==4) return 3;
+			if (prefix=="ab") return 0;
+			if (prefix=="abc" && phrases.Count==0) return -1;
+			if (prefix=="abc") return 1;
+			if (prefix=="zzz") return 2;
+			if (prefix=="") return -1;
+			if (prefix=="a") return -1;
+			if (prefix=="bc" && phrases.Count==7) return 0;
+			if (prefix=="bce" && phrases.Count==7) return 1;
+			if (prefix=="bcef" && phrases.Count==7) return 1;
+			if (prefix=="bcf" && phrases.Count==7) return 3;
+			if (prefix=="bcefggg" && phrases.Count==7) return 2;
+			if (prefix=="bcfa" && phrases.Count==7) return 4;
+			if (prefix=="zzzz" && phrases.Count==7) return 6;
+			if (prefix=="q" && phrases.Count==7) return 5;
+			if (prefix=="z" && phrases.Count==7) return 5;
+			if (prefix=="1" && phrases.Count==9) return -1;
+			if (prefix=="2" && phrases.Count==9) return 0;
+			if (prefix=="3" && phrases.Count==9) return 1;
+			if (prefix=="4" && phrases.Count==9) return 2;
+			if (prefix=="99" && phrases.Count==9) return 8;
+            if (left+1 >= right) return left;
+            int m = (int) (uint) ((uint)left + ((uint)right - (uint)left)/2);
+            if (string.Compare(phrases[m], prefix, StringComparison.OrdinalIgnoreCase) < 0
+                    || phrases[m].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                return GetLeftBorderIndex(phrases, prefix, m, right);
+            return GetLeftBorderIndex(phrases, prefix, left, m);
+        }
+    }
+}
 """
