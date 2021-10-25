d90d419687abf4ac736e5967267b6d077f54c2e4
IvanDemin3467 <IvanDemin3467@gmail.com>
2021-10-25 15:16:02 +0600

    04-right-border-task - works good

--- ulearn-me/01-Search-and-sort/04-right-border-task.py
+++ ulearn-me/01-Search-and-sort/04-right-border-task.py
@@ -32,33 +32,38 @@ namespace Autocomplete
 def get_right_border_index(phrases: [str], prefix: str, left: int, right: int):
     # left = 0
     # right = len(phrases) - 1
-    if prefix == "":
-        return right  # A case of empty prefix
-    if right == 0:
-        return 0  # A case of empty list
-    if phrases[right-1].startswith(prefix):
-        return right  # Check right border
-    if phrases[right-2].startswith(prefix):
-        return right-1  # Check right border
-    tr = right
+    # if prefix == "":
+    #     return right  # A case of empty prefix
+    # if right == 0:
+    #     return 0  # A case of empty list
+    # if phrases[right-1].startswith(prefix):
+    #     return right  # Check right border
+    # if phrases[right-2].startswith(prefix):
+    #     return right-1  # Check right border
+    # tr = right
     while right - left > 0:
         m = (left + (right - left) // 2)
         if m == -1:
-            return 0  # A case of empty Phrases list
+            break  # A case of empty Phrases list
         print(f"\nleft=={left}, right=={right}, m=={m}")
-        print(f"phrases[m]=={phrases[m]}, (phrases[m] < prefix) == {phrases[m] < prefix}")
-        if phrases[m] < prefix:
+        print(f"phrases[m]=={phrases[m]}, (prefix < phrases[m]) == {prefix < phrases[m]} (phrases[m].startswith(prefix)) == {phrases[m].startswith(prefix)}")
+        if phrases[m].startswith(prefix):
             left = m + 1
             print(f"left = m + 1 == {left}")
-        else:
+            continue
+        if prefix < phrases[m]:
             right = m
             print(f"right = m == {right}")
-    if right == tr:
-        return right  # A case of "Nothing found"
-    if phrases[right].startswith(prefix):
-        return right + 1
-    else:
-        return right
+        else:
+            left = m + 1
+            print(f"left = m + 1 == {left}")
+    # if right == tr:
+    #     return right  # A case of "Nothing found"
+    # if phrases[right].startswith(prefix):
+    #     return right + 1
+    # else:
+    #     return right
+    return right
 
 
 # array = ["a", "ab", "abc"]
@@ -68,7 +73,7 @@ array = ["a", "bcd", "bcefg", "bcefh", "bcf", "bcff", "zzz"]
 
 # to_search = "abb"
 # to_search = ""
-to_search = "a"
+to_search = "bce"
 print(get_right_border_index(array, to_search, -1, len(array)))
 
 """
@@ -82,31 +87,24 @@ namespace Autocomplete
     {
         public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
         {
-			if (prefix == "") return right;  // A case of empty prefix
-			if (right == 0) return 0;  // A case of empty list
-			var tr = right;
-			if (phrases[tr-1].StartsWith(prefix, StringComparison.OrdinalIgnoreCase)) 
-				return tr;
-			if (phrases[tr-2].StartsWith(prefix, StringComparison.OrdinalIgnoreCase)) 
-				return tr-1;
+			//if (prefix == "") return right;  // A case of empty prefix
+			//if (right == 0) return 0;  // A case of empty list
 			while (right - left > 0)
 			{
 				int m = (left + (right - left)/2);
-				if (m == -1) return 0;  // A case of empty Phrases list
-				if (string.Compare(phrases[m], prefix, StringComparison.OrdinalIgnoreCase) < 0)
-					left = m + 1;
-				else right = m;
+				if (m == -1) break;  // To evade an exception "Index out of borders"
 				if (phrases[m].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
-					return right + 1;
+				{
+					left = m + 1;
+					continue;
+				}
+				if (string.Compare(prefix, phrases[m], StringComparison.OrdinalIgnoreCase) < 0)
+					right = m;
+				else left = m + 1;
 			}
-			if (right == tr) return right;  // A case of "Nothing found"
-			
-			
-			if (phrases[right].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
-				return right + 1;
-			else
-				return right;
+			return right;
         }
     }
 }
+
 """
